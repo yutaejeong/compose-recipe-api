ae859f7adc079ba15dfa2b63ff86040f0b580919
유태정 <qmamsm123@naver.com>
2025-05-16 12:10:54 +0900

    봄이었다

---
+++ src/main/java/me/taejeong/compose/recipe/ComposeRecipeApiApplication.java
@@ -0,0 +1,13 @@
+package me.taejeong.compose.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposeRecipeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComposeRecipeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/taejeong/compose/recipe/ComposeRecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.taejeong.compose.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeRecipeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
